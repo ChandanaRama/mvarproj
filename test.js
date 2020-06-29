a3e5d76e79fa77df4c74049bdb2ae74365226496
ANVESH VALLAKATI <anvesh86@ANVESHs-MacBook-Air.local>
2020-06-29 09:56:44 -0400

    123

--- test.js
+++ test.js
@@ -1,3 +1,17 @@
 function test(){
-    console.log()
+    console.log('te;st')
 }
+
+
+//clone 
+//make our changes in local
+//stage our changes
+//commit staged changes
+//push
+
+// pull if we wnat to get code
+
+//create new branch for new requirements
+
+//after dev and test we merge it to master branch
+
